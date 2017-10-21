9e14820aa36d3e7c9c9c0e30afc221a3f4632ac9
ivicelig <ivicelig@gmail.com>
2017-10-21 14:07:47 +0200

    Added database.

--- FiremanPro/database/src/main/java/com/project/test/database/MainDatabase.java
+++ FiremanPro/database/src/main/java/com/project/test/database/MainDatabase.java
@@ -1,11 +1,16 @@
 package com.project.test.database;
 
+import com.raizlabs.android.dbflow.annotation.Database;
+
 /**
  * Created by Ivica on 21.10.2017..
  */
+@Database(name = MainDatabase.NAME, version = MainDatabase.VERSION )
 
 public class MainDatabase {
 
+    public static final String NAME = "FiremanProDatabase";
+    public static final int VERSION = 1;
 
 
 }
