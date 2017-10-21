8e7587045d8f90ceeb59e7e5f54379bdbf13f781
bukovecn <nikbukove@foi.hr>
2017-10-21 19:13:21 +0200

    Revert "Početna aktivnost dodana (za pretragu kuća)"
    This reverts commit 823ad8804b321367011f6f065fe78bc45d2e1ef2.

--- FiremanPro/app/src/main/java/com/project/air/firemanpro/MainActivity.java
+++ FiremanPro/app/src/main/java/com/project/air/firemanpro/MainActivity.java
@@ -1,12 +1,7 @@
 package com.project.air.firemanpro;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button search = (Button) findViewById(R.id.buttonSearching);
-
-        search.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent Intent = new Intent(view.getContext(), SearchingResultsActivity.class);
-                view.getContext().startActivity(Intent);
-            }
-        });
     }
 }
