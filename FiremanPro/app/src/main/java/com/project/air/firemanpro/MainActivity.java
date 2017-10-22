9b198d40b84dbed33257782db6e80d40a11bed51
bukovecn <nikbukove@foi.hr>
2017-10-22 17:22:45 +0200

    Main Activity design + button on new Activity
    MainActivity design completed, new activity added: SearchingResultsActivity -
     unfinished!

--- FiremanPro/app/src/main/java/com/project/air/firemanpro/MainActivity.java
+++ FiremanPro/app/src/main/java/com/project/air/firemanpro/MainActivity.java
@@ -1,7 +1,10 @@
 package com.project.air.firemanpro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
@@ -14,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FlowManager.init(new FlowConfig.Builder(this).build());
 
+        Button search = (Button) findViewById(R.id.buttonSearching);
+        search.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent Intent = new Intent(view.getContext(), SearchingResultsActivity.class);
+                view.getContext().startActivity(Intent);
+            }
+        });
     }
 }
---
+++ FiremanPro/app/src/main/java/com/project/air/firemanpro/SearchingResultsActivity.java
@@ -0,0 +1,17 @@
+package com.project.air.firemanpro;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.Bind;
+
+public class SearchingResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searching_results);
+
+    }
+}
