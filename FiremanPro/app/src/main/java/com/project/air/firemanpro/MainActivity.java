0f054456536f0ff469c02a3654a2efc1fba39289
Zoran Hrnčić <zorhrncic@foi.hr>
2017-10-22 13:54:07 +0200

    Ispravak greškom "zaprljanog" repozitorija
    -vračena je verzija projekta izrađena na dan 21.10.2017. na Commit
    naziva "Added database"

--- FiremanPro/app/src/main/java/com/project/air/firemanpro/MainActivity.java
+++ FiremanPro/app/src/main/java/com/project/air/firemanpro/MainActivity.java
@@ -3,11 +3,16 @@ package com.project.air.firemanpro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FlowManager.init(new FlowConfig.Builder(this).build());
+
     }
 }
