8eb19f87e8382d9e98df7bf75bf081fa112652b5
ivicelig <ivicelig@gmail.com>
2017-10-26 01:28:25 +0200

    Added onClick events on RecycleList items that opens profile of individual house.
    Added onClick events on RecycleList items that opens profile - INCOMPLETE

--- FiremanPro/app/src/main/java/com/project/air/firemanpro/holders/SearchingViewHolder.java
+++ FiremanPro/app/src/main/java/com/project/air/firemanpro/holders/SearchingViewHolder.java
@@ -1,16 +1,20 @@
 package com.project.air.firemanpro.holders;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.project.air.firemanpro.ProfilActivity;
 import com.project.air.firemanpro.R;
 import com.project.test.database.Entities.House;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Nikol on 23.10.2017..
@@ -21,13 +25,28 @@ public class SearchingViewHolder extends RecyclerView.ViewHolder {
     public TextView adressInfo;
     public TextView name;
     public ImageView housePicture;
+    public TextView ID;
 
+
+    View mItemView;
 
     public SearchingViewHolder(View v) {
         super(v);
         adressInfo = (TextView) v.findViewById(R.id.tv_info);
         name = (TextView) v.findViewById(R.id.tv_name);
         housePicture = (ImageView) v.findViewById(R.id.imageResult1);
+        ID = (TextView) v.findViewById(R.id.tv_ID);
+        mItemView = itemView;
+        ButterKnife.bind(this, itemView);
+    }
+    @OnClick
+    public void clikcOnItem(){
+
+        Intent intent = new Intent(itemView.getContext(), ProfilActivity.class);
+        intent.putExtra("EXTRA_SESSION_ID", ID.getText()); // umjesto 01 prosljediš ID kuće
+        itemView.getContext().startActivity(intent);
+
+
     }
 
 
