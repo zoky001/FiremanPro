1f66581c891be7e011ae98013c1a619e5c79f56c
ivicelig <ivicelig@gmail.com>
2018-01-18 17:09:26 +0100

    Changeing Apche POI API to another version.

--- FiremanPro/apache_poi/src/main/java/com/project/test/apache_poi/CreateDocument.java
+++ FiremanPro/apache_poi/src/main/java/com/project/test/apache_poi/CreateDocument.java
@@ -1,8 +1,21 @@
 package com.project.test.apache_poi;
 
+
+import org.apache.poi.common.usermodel.HyperlinkType;
 import java.io.File;
 import java.io.FileOutputStream;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Hyperlink;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
 
 public class CreateDocument {
 
